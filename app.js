0d635900a36100d253d00fca499fa9b69bfa6b1f
Emilie <EmilieMontpre@outlook.com>
2023-01-27 12:35:17 +0100

    Ajouts des currencies

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const Fixer = {
     data(){
         return{
-
+            Nbr:"",
+            Currencies:"",
+            CurrenciN:"",
+            CurrenciC:"",
+            Result:"",
         }
+    },
+    mounted(){
+        fetch('https://api.frankfurter.app/currencies')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            this.Currencies=json;
+        })
+    },
+    mounted(){
+        fetch(`'https://api.frankfurter.app/latest?amount=' + Nbr`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            this.Result=json;
+        })
     }
 }
 Vue.createApp(Fixer).mount("#app")
